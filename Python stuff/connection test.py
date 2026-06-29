00fd8d6c59b91dd3fac521313db4014a0234a568
UnknownPhys6 <42011077+UnknownPhys6@users.noreply.github.com>
2026-06-29 16:02:10 -0500

    Update connection test.py
    added comment and changed file to use .env file instead of hardcoding login info

--- Python stuff/connection test.py
+++ Python stuff/connection test.py
@@ -3,15 +3,23 @@ This file is just used for testing locally.
 I know its bad practice to post login credentials to the internet, I'll change it to use .env files later
 """
 import mysql.connector
+import os
+from dotenv import load_dotenv
 
-cnx = mysql.connector.connect(
-    user="developer",
-    password="Group2pass",
-    host="192.168.1.144",
-    database="maintenance_requests",
-    port=3306
-)
+# Load the environment variables from the .env file
+load_dotenv()
 
-print("Connected!")
+try:
+    cnx = mysql.connector.connect(
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        host=os.getenv('DB_HOST'),
+        database=os.getenv('DB_NAME'),
+        port=int(os.getenv('DB_PORT', 3306))  # Defaults to 3306 if DB_PORT isn't set
+    )
 
-cnx.close()
+    print("Connected successfully")
+    cnx.close()
+
+except mysql.connector.Error as err:
+    print(f"Error connecting to the database: {err}")
