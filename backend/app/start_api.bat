9563580e3adcc016ba0343e4a3d33fcf8d212acd
UnknownPhys6 <42011077+UnknownPhys6@users.noreply.github.com>
2026-07-20 23:53:31 -0500

    Update start_api.bat
    removed the second ngrok call because I cant open a tcp tunnel without entering a credit card.

--- backend/app/start_api.bat
+++ backend/app/start_api.bat
@@ -9,8 +9,5 @@ call .venv\Scripts\activate
 
 start "Backend" cmd /k python -m uvicorn api:app --reload
 start "ngrok api access" cmd /k ngrok http 8000
-start "ngrok sql access" cmd /k ngrok tcp 3306
 
-echo.
-echo Backend stopped.
 pause
